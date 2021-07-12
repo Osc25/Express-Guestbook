404877ee04e36f213a3a0dd8fd6283412f09e7b0
Oscar Eduardo <esimio.ice23@gmail.com>
2021-07-12 10:50:53 -0500

    express

---
+++ src/routes/entries.routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const router = Router();
+
+const { renderIndex, renderNewEntry, createNewEntry } = require('../controllers/entries.controller');
+
+router.get('/', )
+
+module.exports = router;
